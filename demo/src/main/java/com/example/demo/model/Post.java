428093d4420cf7a0bc24549330404180a625ed2d
Boseok <sojin0702@gmail.com>
2020-08-09 17:43:14 +0900

    3. 컨트롤러 추가

--- demo/src/main/java/com/example/demo/model/Post.java
+++ demo/src/main/java/com/example/demo/model/Post.java
@@ -1,5 +1,21 @@
 package com.example.demo.model;
 
-public class Post {
+import java.security.Timestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Post {
+    private int id;
+    private String title;
+    private String content;
+    private int userId;
+    private Timestamp createDate; 
 }
