a5f7cbefd8288c5824c6dd39e0a0b0134de47d59
Boseok <sojin0702@gmail.com>
2020-08-09 19:14:37 +0900

    5.ajax 로직 만들기

--- demo/src/main/java/com/example/demo/controller/IndexController.java
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+	    // 응답을 data로 받기위해서 responsebody 추가
+		// (User user)를 이용하면 xxx-form-urlencoded 만 받아진다
+		// (@RequestBody User user)를 이용하면 
+		// jwt 등 공부하기
+	
+	
 	@GetMapping({"","/"})
 	public String index() {
 		return "index";
